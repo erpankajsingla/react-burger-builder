f66cb649f7076d0e4363d56bce00fbeaf2be5fe6
Pankaj Singla <psingla@605.tv>
2020-08-19 13:49:15 -0400

    Burger Builder

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'},
+];
+
+const BuildControls = (props) => {
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label} 
+        addIngredient={() => props.addIngredient(ctrl.type)} 
+        removeIngredient={() => props.removeIngredient(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}/>
+      ))}
+    </div>
+  )
+}
+
+export default BuildControls
+
